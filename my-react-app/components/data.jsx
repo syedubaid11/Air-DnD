380141c394afb84f022605f541798a7e90706665
syedubaid <syedabuubaid@gmail.com>
2024-05-03 22:56:30 +0530

    commit

--- my-react-app/components/data.jsx
+++ my-react-app/components/data.jsx
@@ -7,5 +7,16 @@ export default [
         endDate: "24 Jan, 2021",
         image: "mountfuji.png",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    },
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "22 Feb, 2021",
+        endDate: "28 Feb, 2021",
+        image: "sydney.png",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
     }
+    
+
 ]
