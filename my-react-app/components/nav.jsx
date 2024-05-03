e13bb5f556cadbcdc377d1825aadfb933af80458
syedubaid <syedabuubaid@gmail.com>
2024-05-03 15:13:47 +0530

    commit

---
+++ my-react-app/components/nav.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+export default function Nav(){
+    return (
+        <>
+            <div className='nav--container'>
+                <div className='nav'>
+                    <p>my travel journal.</p>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from '../components/nav'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav></Nav>
+    </>
+    
+  )
+}
+
+export default App
