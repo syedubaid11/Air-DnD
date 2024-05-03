6f4261b8aae262d5ade2d709a6782646dbb7793e
syedubaid <syedabuubaid@gmail.com>
2024-05-03 20:54:14 +0530

    commit

---
+++ my-react-app/components/cards.jsx
@@ -0,0 +1,22 @@
+export default function Card(props){
+    console.log(props.image)
+        return(
+            <div className="card">
+            <img src={`./images/${props.image}`} className="card--image" />
+                <div className="card-stats">
+                    <p className="card-location">{props.location}<span className="card-googlereview">{props.googleMapsUrl}</span></p>
+                    <p className="card-title">
+                        {props.title}
+                    </p>
+
+                    <p className="card-date">{props.startDate}-{props.endDate}</p>
+
+
+
+                    
+                </div>
+
+            </div>
+        )
+
+}
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,14 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from '../components/nav'
+import data from '../components/data'
+import Card from '../components/cards'
 
 
 function App() {
+  const map=data.map((item)=>{
+    return(
+      <Card 
+      key={1}
+      mapurl={item.googleMapsUrl}
+      title={item.title}
+      location= {item.location}
+      startDate={item.startDate}
+      endDate={item.endDate}
+      image={item.image}
+      
+      />
+    )
+  })
   
 
   return (
     <>
     <Nav></Nav>
+    {map}
     </>
     
   )
