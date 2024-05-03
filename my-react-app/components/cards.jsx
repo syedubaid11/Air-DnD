05791b9d2e00124be9c221bcb28ef28165288f04
syedubaid <syedabuubaid@gmail.com>
2024-05-03 21:21:14 +0530

    commit

--- my-react-app/components/cards.jsx
+++ my-react-app/components/cards.jsx
@@ -5,15 +5,8 @@ export default function Card(props){
             <img src={`./images/${props.image}`} className="card--image" />
                 <div className="card-stats">
                     <p className="card-location">{props.location}<span className="card-googlereview">{props.googleMapsUrl}</span></p>
-                    <p className="card-title">
-                        {props.title}
-                    </p>
-
-                    <p className="card-date">{props.startDate}-{props.endDate}</p>
-
-
-
-                    
+                    <p className="card-title">{props.title}</p>
+                    <p className="card-date">{props.startDate}-{props.endDate}</p>     
                 </div>
 
             </div>
